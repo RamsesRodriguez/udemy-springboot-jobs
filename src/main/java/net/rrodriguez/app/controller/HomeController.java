e2cae12b01f2a3f88338569963369c8fd42610e9
ramsesrodriguez <rodriguezgarciaramses@gmail.com>
2020-08-16 15:44:38 +0200

    Questions #15-17 done

--- src/main/java/net/rrodriguez/app/controller/HomeController.java
+++ src/main/java/net/rrodriguez/app/controller/HomeController.java
@@ -1,6 +1,8 @@
 package net.rrodriguez.app.controller;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +11,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	@GetMapping("/list")
+	public String getJobList(Model model) {
+		List<String> list = new LinkedList<String>();
+		list.add("Ingeniero de Sistemas");
+		list.add("Auxiliar de Contabilidad");
+		list.add("Vendedor");
+		list.add("Arquitecto");
+
+		model.addAttribute("jobs", list);
+
+		return "jobList";
+	}
+
 	@GetMapping("/")
 	public String getHome(Model model) {
 
-		model.addAttribute("message", "Bienvenidos a Empleos App");
-		model.addAttribute("fecha", new Date());
+		String name = "Auxiliar de Contabilidad";
+		Date publicationDate = new Date();
+		double salary = 9000.0;
+		boolean prevailing = true;
+
+		model.addAttribute("name", name);
+		model.addAttribute("publicationDate", publicationDate);
+		model.addAttribute("salary", salary);
+		model.addAttribute("prevailing", prevailing);
 
 		return "home";
+
 	}
 
 }
